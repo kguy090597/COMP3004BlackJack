44996280f0f2b7db86d9b16ff13301b5fc71494e
kguy090597 <kguy090597@gmail.com>
2018-09-15 10:51:39 -0400

    Added various tests to the hand test class to ensure that the sum can be correctly added up and whether or no the hand is a bust or is a blackjack

--- src/test/java/blackjack/HandTest.java
+++ src/test/java/blackjack/HandTest.java
@@ -22,4 +22,106 @@ public class HandTest {
 		assertEquals("SA S1 S2", hand.toString());
 	}
 	
+	@Test
+	public void testHand() {
+		Hand hand = new Hand();
+		Card card1 = new Card("S","A");
+		Card card2 = new Card("S","1");
+		Card card3 = new Card("S","2");
+		hand.add(card1);
+		hand.add(card2);
+		hand.add(card3);
+		assertEquals(14,hand.sumHand());
+		assertEquals(false,hand.isBlackJack());
+	}
+	
+	@Test
+	public void testHand21() {
+		Hand hand = new Hand();
+		Card card1 = new Card("S","A");
+		Card card2 = new Card("S","J");
+		Card card3 = new Card("S","Q");
+		hand.add(card1);
+		hand.add(card2);
+		hand.add(card3);
+		assertEquals(21,hand.sumHand());
+		assertEquals(true,hand.isBlackJack());
+	}
+	
+	@Test
+	public void testHandTwoAces() {
+		Hand hand = new Hand();
+		Card card1 = new Card("S","A");
+		Card card2 = new Card("H","A");
+		Card card3 = new Card("S","J");
+		hand.add(card1);
+		hand.add(card2);
+		hand.add(card3);
+		assertEquals(12,hand.sumHand());
+		assertEquals(false,hand.isBlackJack());
+	}
+	
+	@Test
+	public void testHandFourAces() {
+		Hand hand = new Hand();
+		Card card1 = new Card("S","A");
+		Card card2 = new Card("H","A");
+		Card card3 = new Card("C","A");
+		Card card4 = new Card("D","A");
+		Card card5 = new Card("S","9");
+		hand.add(card1);
+		hand.add(card2);
+		hand.add(card3);
+		hand.add(card4);
+		hand.add(card5);
+		assertEquals(13,hand.sumHand());
+		assertEquals(false,hand.isBlackJack());
+	}
+	
+	@Test
+	public void testHandFourAces21() {
+		Hand hand = new Hand();
+		Card card1 = new Card("S","A");
+		Card card2 = new Card("H","A");
+		Card card3 = new Card("C","A");
+		Card card4 = new Card("D","A");
+		Card card5 = new Card("S","7");
+		hand.add(card1);
+		hand.add(card2);
+		hand.add(card3);
+		hand.add(card4);
+		hand.add(card5);
+		assertEquals(21,hand.sumHand());
+		assertEquals(true,hand.isBlackJack());
+	}
+	
+	@Test
+	public void testSumHandTwoAces21() {
+		Hand hand = new Hand();
+		Card card1 = new Card("S","A");
+		Card card2 = new Card("H","A");
+		Card card3 = new Card("S","9");
+		hand.add(card1);
+		hand.add(card2);
+		hand.add(card3);
+		assertEquals(21,hand.sumHand());
+		assertEquals(true,hand.isBlackJack());
+	}
+	
+	@Test
+	public void testHandOver() {
+		Hand hand = new Hand();
+		Card card1 = new Card("S","9");
+		Card card2 = new Card("H","9");
+		Card card3 = new Card("D","9");
+		hand.add(card1);
+		hand.add(card2);
+		hand.add(card3);
+		assertEquals(27,hand.sumHand());
+		assertEquals(false,hand.isBlackJack());
+		assertEquals(true,hand.isOver());
+	}
+	
+	
+
 }
